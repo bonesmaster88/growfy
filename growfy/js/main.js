2fcb68442f74e4711ab71865bebdcaf8d570b69a
bonesmaster88 <rudovasvit@gmail.com>
2025-07-19 00:01:58 +0400

    Add files via upload

---
+++ growfy/js/main.js
@@ -0,0 +1,17 @@
+// burger-menu
+document.addEventListener('click', documentActions)
+
+function documentActions(e) {
+	const targetElement = e.target
+
+	if (targetElement.closest('.icon-menu')) {
+		document.body.classList.toggle('menu-open')
+	}
+
+
+
+
+
+
+
+}
